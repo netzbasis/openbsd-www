c2eadee69ddfe914791020f6a350f990ed72b0a2
Ben <programmer@netzbasis.de>
2018-05-13 08:19:30 +0200

    sync with openbsd-current

--- build/openssh/releases.pl
+++ build/openssh/releases.pl
@@ -10,6 +10,8 @@ use warnings 'all';
 
 my $txtdir = "../../openssh/txt/";
 my $bzurl = "https://bugzilla.mindrot.org/show_bug.cgi?id=";
+my $manurl = "https://man.openbsd.org/";
+my $dlurl = "https://cdn.openbsd.org/pub/OpenBSD/OpenSSH";
 
 # These days we commit the release notes on the day of release.  For those,
 # we use the date the release notes were first committed.
@@ -120,11 +122,12 @@ sub output_release
 	my ($rel, $date) = @_;
 	my $fixdate = 0;
 	my @notes;
+	my $file = "$txtdir/release-$rel";
 
-	open(my $fh, '<', "$txtdir/release-$rel") || die;
+	open(my $fh, '<', $file) || die "can't open '$file': $!";
 	while (<$fh>) {
 		$fixdate = 1 if (/has just been released/);
-		if ($fixdate = 1) {
+		if ($fixdate) {
 			s|has just been released|was released on $date|;
 			s|It will be available from|It is available from|;
 			s|http://www.openssh.com/ shortly.|https://www.openssh.com/.|;
@@ -134,6 +137,16 @@ sub output_release
 		# expand bugzilla references into URLs.
 		s|bz#(\d+)|<a href='$bzurl$1'>bz#$1</a>|g;
 		s|bz #(\d+)|<a href='$bzurl$1'>bz #$1</a>|g;
+
+		# expand man page references into URLs.
+		my $manpages ="ssh|sshd|scp|sftp|sftp-server|ssh-keygen|" .
+		    "ssh-agent|ssh_config|sshd_config|moduli";
+		s@($manpages)\((\d)\)@<a href='$manurl$1.$2'>$1($2)</a>@g;
+
+		# download links
+		s|(openssh-[\d\.]+.tar.gz)|<a href='$dlurl/$1'>$1</a>|g;
+		s|(openssh-[\d\.]+p\d.tar.gz)|<a href='$dlurl/portable/$1'>$1</a>|g;
+
 		print $_;
 	}
 	close($fh);
